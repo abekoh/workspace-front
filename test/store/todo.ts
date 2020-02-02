7f234ccc3b3689e9b053b0639be270571fda6e59
Kotaro Abe <abekoh.bcky@gmail.com>
2020-02-02 18:03:29 +0900

    update

--- test/store/todo.ts
+++ test/store/todo.ts
@@ -1,5 +1,20 @@
+import {createLocalVue} from '@vue/test-utils'
+import Vuex from 'vuex'
+import Todo from '@/store/todo'
+
 const sum = (x, y) => x + y
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
+})
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+test('todo.add', () => {
+  const state = {
+    tasks: [{taskId: 1}]
+  }
+  Todo.mutations.updateTasks(state)
+  expect(state.tasks).toBe(1)
 })
