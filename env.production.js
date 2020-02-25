983184f430a2f8f08c6dee73db10d3f455d91e28
Kotaro Abe <abekoh.bcky@gmail.com>
2020-02-25 22:44:23 +0900

    環境別設定追加

---
+++ env.production.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // TODO: 修正する
+  apiBaseUrl: 'http://localhost:8080/api/v1/'
+}
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+const environment = process.env.NODE_ENV || 'development';
+const envSet = require(`./env.${environment}.js`);
 
 export default {
   mode: 'universal',
@@ -7,18 +9,18 @@ export default {
   head: {
     title: process.env.npm_package_name || '',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: {color: '#fff'},
   /*
   ** Global CSS
   */
@@ -60,7 +62,7 @@ export default {
   },
   proxy: {
     '/todo': {
-      target: 'http://localhost:8080/api/v1/'
+      target: envSet.apiBaseUrl
     }
   }
 }
